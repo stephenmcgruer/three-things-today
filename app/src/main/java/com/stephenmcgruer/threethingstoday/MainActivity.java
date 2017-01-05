4bf6350277c0d462506e10c867fbcaecf0045714
Stephen McGruer <stephen.mcgruer@gmail.com>
2017-01-05 23:29:43 +0000

    Basic app layout
    This is far from final, but shows the barebones of three text entry areas based
    on a given date. Currently including a 'Submit' button to save changes, though
    this should likely be replaced by auto-save after text is edited.

--- app/src/main/java/com/stephenmcgruer/threethingstoday/MainActivity.java
+++ app/src/main/java/com/stephenmcgruer/threethingstoday/MainActivity.java
@@ -2,6 +2,8 @@ package com.stephenmcgruer.threethingstoday;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showDatePickerDialog(View v) {
+        // TODO(smcgruer): Implement.
+        Toast.makeText(getApplicationContext(), "showDatePickerDialog", Toast.LENGTH_SHORT).show();
+    }
+
+    public void submitThreeThings(View v) {
+        // TODO(smcgruer): Implement.
+        Toast.makeText(getApplicationContext(), "submitThreeThings", Toast.LENGTH_SHORT).show();
     }
 }
