4b2df59c36e9b969e14d517ee9cca2113cce650b
Stephen McGruer <stephen.mcgruer@gmail.com>
2017-01-06 14:47:33 +0000

    Use AsyncTask for database interactions
    This is possibly unnecessary due the simplicity of the database interactions,
    but to be correct they should be moved off of the main thread.

--- app/src/main/java/com/stephenmcgruer/threethingstoday/MainActivity.java
+++ app/src/main/java/com/stephenmcgruer/threethingstoday/MainActivity.java
@@ -4,9 +4,10 @@ import android.app.DatePickerDialog;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -41,8 +42,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
         updateDateText();
 
-        // TODO(smcgruer): Should be async?
-        updateThreeThingTexts();
+        saveThreeThingsToDatabase();
     }
 
     @Override
@@ -62,8 +62,6 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         String secondThingText = getTextFromEditText(R.id.second_edit_text);
         String thirdThingText = getTextFromEditText(R.id.third_edit_text);
 
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
-
         ContentValues values = new ContentValues();
         values.put(ThreeThingsEntry.COLUMN_NAME_YEAR, mSelectedYear);
         values.put(ThreeThingsEntry.COLUMN_NAME_MONTH, mSelectedMonth);
@@ -72,7 +70,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         values.put(ThreeThingsEntry.COLUMN_NAME_SECOND_THING, secondThingText);
         values.put(ThreeThingsEntry.COLUMN_NAME_THIRD_THING, thirdThingText);
 
-        db.replace(ThreeThingsEntry.TABLE_NAME, null, values);
+        new DatabaseWriteTask(mDbHelper).execute(values);
     }
 
     @Override
@@ -82,7 +80,7 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         mSelectedDayOfMonth = dayOfMonth;
 
         updateDateText();
-        updateThreeThingTexts();
+        saveThreeThingsToDatabase();
     }
 
     private void updateDateText() {
@@ -91,52 +89,109 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         dateTextView.setText(mSelectedYear + "/" + mSelectedMonth + "/" + mSelectedDayOfMonth);
     }
 
-    private void updateThreeThingTexts() {
-        String firstThing = "";
-        String secondThing = "";
-        String thirdThing = "";
-
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-        String[] projection = {
-                ThreeThingsEntry.COLUMN_NAME_FIRST_THING,
-                ThreeThingsEntry.COLUMN_NAME_SECOND_THING,
-                ThreeThingsEntry.COLUMN_NAME_THIRD_THING
-        };
-        String selection = ThreeThingsEntry.COLUMN_NAME_YEAR + " = ? AND " +
-                ThreeThingsEntry.COLUMN_NAME_MONTH + " = ? AND " +
-                ThreeThingsEntry.COLUMN_NAME_DAY_OF_MONTH + " = ?";
-        String[] selectionArgs = {
-                Integer.toString(mSelectedYear),
-                Integer.toString(mSelectedMonth),
-                Integer.toString(mSelectedDayOfMonth)
-        };
-
-        Cursor cursor = db.query(
-                ThreeThingsEntry.TABLE_NAME,
-                projection,
-                selection,
-                selectionArgs,
-                null,
-                null,
-                null);
-
-        if (cursor.moveToNext()) {
-            firstThing = cursor.getString(cursor.getColumnIndexOrThrow(ThreeThingsEntry.COLUMN_NAME_FIRST_THING));
-            secondThing = cursor.getString(cursor.getColumnIndexOrThrow(ThreeThingsEntry.COLUMN_NAME_SECOND_THING));
-            thirdThing = cursor.getString(cursor.getColumnIndexOrThrow(ThreeThingsEntry.COLUMN_NAME_THIRD_THING));
-        }
-
+    private void saveThreeThingsToDatabase() {
         EditText firstThingEditText = (EditText) findViewById(R.id.first_edit_text);
         EditText secondThingEditText = (EditText) findViewById(R.id.second_edit_text);
         EditText thirdThingEditText = (EditText) findViewById(R.id.third_edit_text);
 
-        firstThingEditText.setText(firstThing);
-        secondThingEditText.setText(secondThing);
-        thirdThingEditText.setText(thirdThing);
+        DatabaseReadTask readerTask = new DatabaseReadTask(
+                mDbHelper, firstThingEditText, secondThingEditText, thirdThingEditText);
+        readerTask.execute(
+                Integer.toString(mSelectedYear),
+                Integer.toString(mSelectedMonth),
+                Integer.toString(mSelectedDayOfMonth));
     }
 
     private String getTextFromEditText(int resourceId) {
         EditText editText = (EditText) findViewById(resourceId);
         return editText.getText().toString();
+    }
+
+    private static class DatabaseWriteTask extends AsyncTask<ContentValues, Void, Long> {
+
+        private final ThreeThingsDbHelper mDbHelper;
+
+        public DatabaseWriteTask(ThreeThingsDbHelper dbHelper) {
+            mDbHelper = dbHelper;
+        }
+
+        @Override
+        protected Long doInBackground(ContentValues... values) {
+            if (values.length < 1) {
+                return -1L;
+            }
+
+            SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+            return db.replace(ThreeThingsEntry.TABLE_NAME, null, values[0]);
+        }
+    }
+
+    private static class DatabaseReadTask extends AsyncTask<String, Void, String[]> {
+
+        private final ThreeThingsDbHelper mDbHelper;
+
+        private final EditText mFirstThingEditText;
+        private final EditText mSecondThingEditText;
+        private final EditText mThirdThingEditText;
+
+        public DatabaseReadTask(ThreeThingsDbHelper dbHelper, EditText firstThingEditText,
+                                EditText secondThingEditText, EditText thirdThingEditText) {
+            mDbHelper = dbHelper;
+
+            mFirstThingEditText = firstThingEditText;
+            mSecondThingEditText = secondThingEditText;
+            mThirdThingEditText = thirdThingEditText;
+        }
+
+        @Override
+        protected String[] doInBackground(String... selectionArgs) {
+            // If there is no current results for the selection args, we just return empty things.
+            String[] results = {
+                    "",
+                    "",
+                    ""
+            };
+
+            String[] projection = {
+                    ThreeThingsEntry.COLUMN_NAME_FIRST_THING,
+                    ThreeThingsEntry.COLUMN_NAME_SECOND_THING,
+                    ThreeThingsEntry.COLUMN_NAME_THIRD_THING
+            };
+            String selection = ThreeThingsEntry.COLUMN_NAME_YEAR + " = ? AND " +
+                    ThreeThingsEntry.COLUMN_NAME_MONTH + " = ? AND " +
+                    ThreeThingsEntry.COLUMN_NAME_DAY_OF_MONTH + " = ?";
+
+            SQLiteDatabase db = mDbHelper.getReadableDatabase();
+            Cursor cursor = db.query(
+                    ThreeThingsEntry.TABLE_NAME,
+                    projection,
+                    selection,
+                    selectionArgs,
+                    null,
+                    null,
+                    null);
+
+            if (cursor.moveToNext()) {
+                results[0] = cursor.getString(cursor.getColumnIndexOrThrow(ThreeThingsEntry.COLUMN_NAME_FIRST_THING));
+                results[1] = cursor.getString(cursor.getColumnIndexOrThrow(ThreeThingsEntry.COLUMN_NAME_SECOND_THING));
+                results[2] = cursor.getString(cursor.getColumnIndexOrThrow(ThreeThingsEntry.COLUMN_NAME_THIRD_THING));
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void onPostExecute(String[] strings) {
+            super.onPostExecute(strings);
+
+            if (strings.length < 3) {
+                return;
+            }
+
+            mFirstThingEditText.setText(strings[0]);
+            mSecondThingEditText.setText(strings[1]);
+            mThirdThingEditText.setText(strings[2]);
+        }
     }
 }
